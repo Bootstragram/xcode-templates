ca843d80c7eaea0a0ed7ddd986e12b3321d06ce2
Mickaël Floc’hlay <mickael@flochlay.com>
2018-03-23 10:41:27 +0100

    Add Quick & Swift file templates

---
+++ Quick File.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+@testable import ___PACKAGENAMEASIDENTIFIER___
+
+class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
+    override func spec() {
+        describe("my spec") {
+            it("has a dummy test") {
+                expect(true).to(beTrue())
+            }
+        }
+    }
+}
